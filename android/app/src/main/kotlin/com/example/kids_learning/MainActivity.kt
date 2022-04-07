13f6e3a49aa550e53aaf3290cc0f2093d112e873
m-acosta <michaeldacosta2013@gmail.com>
2022-04-06 18:50:33 -0700

    Added support for Firebase

--- android/app/src/main/kotlin/com/example/kids_learning/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.kids_learning
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'firebase_options.dart';
 import 'home.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
       .then((value) => runApp(MyApp()));
